d47f1198f9b1f8aec3b852004762907aa3e208e5
kamyar888 <145184299+kamyar888@users.noreply.github.com>
2026-05-31 21:25:26 +0330

    after reedeman i going to github and get my project tanks git hub :)

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from blog.models import Post
+class PostAdmin(admin.ModelAdmin):
+        date_hierarchy = 'created_date'
+        empty_value_display = '---'
+        list_display = ('title','counted_views','published_date', 'status', 'created_date', 'updated_date')
 # Register your models here.
-admin.site.register(Post)
+admin.site.register(Post,PostAdmin)
