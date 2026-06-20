1126301c56ebaa231b6eb2bb892d1c1eebd437f2
kamyar888 <145184299+kamyar888@users.noreply.github.com>
2026-06-20 09:10:33 +0330

    categories post componentt is complete

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -14,8 +14,19 @@ def function():
 @register.filter
 def snip(value,args=20):
     return value[:args]
-@register.inclusion_tag("popularpost.html")
+@register.inclusion_tag("blog/blog-popular-post.html")
 def popularpost():
     from blog.models import Post
-    posts = Post.objects.filter(status=1).order_by("-published_date")[:2]
+    posts = Post.objects.filter(status=1).order_by("-published_date")[:3]
     return {"posts":posts}
+@register.inclusion_tag("blog/blog-post-categories.html")
+def postcategories():
+    from blog.models import category
+    from blog.models import Post
+    posts = Post.objects.filter(status=1)
+    categories = category.objects.all()
+    cat_dict={}
+    for name in categories:
+        cat_dict[name]=posts.filter(category=name).count()
+    return {"categories":cat_dict}
+    
